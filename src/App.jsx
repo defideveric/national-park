6aa566568f7e5ee096ec8ee4f69a86f163f22219
erichudson <defideveric@gmail.com>
2023-09-11 22:39:28 -0400

    Just need to complete the footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Hero from "./components/Hero/Hero";
 import Cards from "./components/Cards/Cards";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Hero/>
       <Cards/>
+      <Footer/>
    </main>
   )
 }
